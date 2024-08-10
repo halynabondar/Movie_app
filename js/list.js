146528b209e54e868df58bb30fa34c29d70fea52
Halyna Bondar <galya.marusyak@gmail.com>
2024-08-10 20:29:44 +0200

    Address PR comments

--- js/list.js
+++ js/list.js
@@ -1,8 +1,12 @@
 function fetchAllMovies(callback) {
     fetch("https://raw.githubusercontent.com/halynabondar/halynabondar.github.io/main/Data/movieApp.json")
-        .then(response => {
-            response.json().then(moviesList => {
-                callback(moviesList);
-            })
+        .then((response) => {
+            return response.json();
         })
+        .then((moviesList) => {
+            callback(moviesList);
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
 }
