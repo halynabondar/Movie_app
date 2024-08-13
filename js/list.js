2d0b75fa05753e0b06c005e032103289f5741e26
Halyna Bondar <galya.marusyak@gmail.com>
2024-08-13 12:13:16 +0200

    Changes added

--- js/list.js
+++ js/list.js
@@ -9,13 +9,3 @@ async function fetchAllMovies() {
     }
     return data;
 }
-//
-// function fetchAllMovies() {
-//     return fetch("https://raw.githubusercontent.com/halynabondar/halynabondar.github.io/main/Data/movieApp.json")
-//         .then((response) => {
-//             return response.json();
-//         })
-//         .catch(error => {
-//             console.error('There has been a problem with your fetch operation:', error);
-//         });
-// }
